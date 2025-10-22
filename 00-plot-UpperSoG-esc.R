4c922e1e9f6960e4d0fda81770f28975045a3fee
Holt <Carrie.Holt@dfo-mpo.gc.ca>
2025-10-22 11:34:49 -0700

    revised escapement time-series plots to include missing years

--- 00-plot-UpperSoG-esc.R
+++ 00-plot-UpperSoG-esc.R
@@ -26,19 +26,21 @@ filtered_dfa <- dfa %>%
   mutate(IsUsed = ifelse(year < 2005, "no", "yes")) %>%
   select(year, escapement, pop, IsUsed)
 
-adam_dfa <- filtered_dfa %>% filter(pop == "Adam")
-full_years_adam <- data.frame(year = seq(min(adam_dfa$year), max(adam_dfa$year)))
-df_complete <- left_join(full_years_adam,adam_dfa)
+# Add missing years to Nimpkish, where monitoring was not possible (2021,2022)
 
 nimpkish_dfa <- filtered_dfa %>% filter(pop == "Nimpkish")
 full_years_nimpkish <- data.frame(year = seq(min(nimpkish_dfa$year), max(nimpkish_dfa$year)))
 df_complete_N <- left_join(full_years_nimpkish,nimpkish_dfa) %>%
   mutate(IsUsed = ifelse(year < 2005, "no", "yes")) %>%
+  mutate(pop = "Nimpkish")
 
-# df_complete$pop <- "Nimpkish"
-# mutate(IsUsed = ifelse(year < 2005, "no", "yes")) %>%
-## Need to make years consecutive. For Nimpkish there are missing years .e.g, 2021,2022 that are not recorded
-## still needs to be done
+
+
+filtered_dfa_complete <- filtered_dfa %>%
+  # Remove rows for Nimpkish
+  filter(!pop %in% df_complete_N$pop) %>%
+  # Add the updated rows
+  bind_rows(df_complete_N)
 
 
 # Load the Excel file
@@ -61,7 +63,7 @@ filtered_dfb <- dfb %>%
   mutate(IsUsed = ifelse(year < 2005, "no", "yes")) %>%
   select(year, escapement, pop, IsUsed)
 
-filtered_df <- bind_rows(filtered_dfa, filtered_dfb)
+filtered_df <- bind_rows(filtered_dfa_complete, filtered_dfb)
   # right_join(
   #   full_table %>% filter(Age == 1) %>% select(BROOD_YEAR),
   #   by = c("year" = "BROOD_YEAR")
